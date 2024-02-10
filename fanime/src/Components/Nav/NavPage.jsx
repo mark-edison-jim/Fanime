fceec06d3222753c6c45ed26e0337678b98755ab
Mark Jim <markedisonjim@gmail.com>
2024-02-10 17:28:45 +0800

    changed navpage 1

--- fanime/src/Components/Nav/NavPage.jsx
+++ fanime/src/Components/Nav/NavPage.jsx
@@ -2,16 +2,21 @@ import React, {useState} from "react";
 import './NavPage.css';
 import {LoginForm} from '../LoginForm/LoginForm';
 import {RegisterForm} from '../RegisterForm/RegisterForm';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { styled, css } from '@mui/system';
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoSearch } from "react-icons/io5";
-import { Modal as BaseModal} from '@mui/base/Modal';
+import Modal from '@mui/material/Modal';
+import Backdrop from '@mui/material/Backdrop';
 
 export const NavPage = () => {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
+    const [open, setOpen] = useState(false);
+    const handleRegOpen = () => {
+      setCurrentForm('register');
+      setOpen(true);
+    };
+    const handleLoginOpen = () => {
+      setCurrentForm('login');
+      setOpen(true);
+    };
     const handleClose = () => setOpen(false);
     const [currentForm, setCurrentForm] = useState('login');
 
@@ -43,11 +48,10 @@ export const NavPage = () => {
             <li><a href="#about">About</a></li>
           </ul>
         </div>
-        <div>
-            <button id='signupBtn' onClick={handleOpen}>Sign-Up</button>
-        </div>
         <form id="app" action="#">
           <div id="searchHolder">
+            <button id='signlogBtn' onClick={handleRegOpen}>Sign-Up</button>
+            <button id='signlogBtn' onClick={handleLoginOpen}>Login</button>
           <div id="search">
             <input id="bar" type="text" placeholder=""></input>
             <button id="iconButton"><IoSearch id="icon" class="fa-solid fa-magnifying-glass"/></button>
@@ -55,136 +59,18 @@ export const NavPage = () => {
           </div>
         </form>
         </nav>
-        <Modal
-        aria-labelledby="unstyled-modal-title"
-        aria-describedby="unstyled-modal-description"
-        open={open}
-        onClose={handleClose}
-        slots={{ backdrop: StyledBackdrop }}
-      >
-        {
-        currentForm === 'login' ? <LoginForm onFormSwitch={toggleForm}/> : <RegisterForm onFormSwitch={toggleForm}/>
-        }
+        <Modal id="modal"
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+        >
+          {currentForm === 'login' ? <LoginForm onFormSwitch={toggleForm}/> : <RegisterForm onFormSwitch={toggleForm}/>}
         </Modal>
         </div>
   )
 }
-const Backdrop = React.forwardRef((props, ref) => {
-    const { open, className, ...other } = props;
-    return (
-      <div
-        className={clsx({ 'base-Backdrop-open': open }, className)}
-        ref={ref}
-        {...other}
-      />
-    );
-  });
-  
-  Backdrop.propTypes = {
-    className: PropTypes.string.isRequired,
-    open: PropTypes.bool,
-  };
 
-  const blue = {
-    200: '#99CCFF',
-    300: '#66B2FF',
-    400: '#3399FF',
-    500: '#007FFF',
-    600: '#0072E5',
-    700: '#0066CC',
-  };
-  
-  const grey = {
-    50: '#F3F6F9',
-    100: '#E5EAF2',
-    200: '#DAE2ED',
-    300: '#C7D0DD',
-    400: '#B0B8C4',
-    500: '#9DA8B7',
-    600: '#6B7A90',
-    700: '#434D5B',
-    800: '#303740',
-    900: '#1C2025',
-  };
-  const Modal = styled(BaseModal)`
-  position: fixed;
-  z-index: 1300;
-  inset: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const StyledBackdrop = styled(Backdrop)`
-  z-index: -1;
-  position: fixed;
-  inset: 0;
-  background-color: rgb(0 0 0 / 0.5);
-  -webkit-tap-highlight-color: transparent;
-`;
-
-const ModalContent = styled('div')(
-  ({ theme }) => css`
-    font-family: 'IBM Plex Sans', sans-serif;
-    font-weight: 500;
-    text-align: start;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    overflow: hidden;
-    background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-    border-radius: 8px;
-    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    box-shadow: 0 4px 12px
-      ${theme.palette.mode === 'dark' ? 'rgb(0 0 0 / 0.5)' : 'rgb(0 0 0 / 0.2)'};
-    padding: 24px;
-    color: ${theme.palette.mode === 'dark' ? grey[50] : grey[900]};
-
-    & .modal-title {
-      margin: 0;
-      line-height: 1.5rem;
-      margin-bottom: 8px;
-    }
-
-    & .modal-description {
-      margin: 0;
-      line-height: 1.5rem;
-      font-weight: 400;
-      color: ${theme.palette.mode === 'dark' ? grey[400] : grey[800]};
-      margin-bottom: 4px;
-    }
-  `,
-);
-
-const TriggerButton = styled('button')(
-  ({ theme }) => css`
-    font-family: 'IBM Plex Sans', sans-serif;
-    font-weight: 600;
-    font-size: 0.875rem;
-    line-height: 1.5;
-    padding: 8px 16px;
-    border-radius: 8px;
-    transition: all 150ms ease;
-    cursor: pointer;
-    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
-    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
-
-    &:hover {
-      background: ${theme.palette.mode === 'dark' ? grey[800] : grey[50]};
-      border-color: ${theme.palette.mode === 'dark' ? grey[600] : grey[300]};
-    }
-
-    &:active {
-      background: ${theme.palette.mode === 'dark' ? grey[700] : grey[100]};
-    }
-
-    &:focus-visible {
-      box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? blue[300] : blue[200]};
-      outline: none;
-    }
-  `,
-);    
 export default NavPage;
