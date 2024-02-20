9d3b85068e0b1a0d3ee1e0e6c46407ddee797cce
Mark Jim <markedisonjim@gmail.com>
2024-02-20 20:37:57 +0800

    migrated to nodejs

---
+++ fanime/app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const server = express();
+
+//Add needed statements
+const data = require('./data');
+
+const bodyParser = require('body-parser')
+server.use(express.json()); 
+server.use(express.urlencoded({ extended: true }));
+
+const handlebars = require('express-handlebars');
+server.set('view engine', 'hbs');
+server.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+server.use(express.static('public'));
+server.use(express.static('Assets'));
+
+server.get('/', function(req, resp){
+    resp.render('main', {
+        layout: 'index',
+        title: 'Main Page',
+        posts: data.posts
+    });
+});
+
+server.get('/profile', function(req, resp){
+    resp.render('profile', {
+        layout: 'profileIndex',
+        title: 'Profile Page',
+        user: data.users.person1,
+    });
+});
+
+server.get('/post/:id/', function(req, resp){
+    resp.render('post', {
+        layout: 'index',
+        title: 'Post Page',
+        post: data.posts[req.params.id]
+    });
+    
+});
+
+server.get('/editpost', function(req, resp){
+    resp.render('editpost', {
+        layout: 'index',
+        title: 'Edit Post Page'
+    });
+    
+});
+
+server.get('/editcomment', function(req, resp){
+    resp.render('editcomment', {
+        layout: 'index',
+        title: 'Edit Comment Page'
+    });
+    
+});
+
+const port = process.env.PORT | 9090;
+server.listen(port, function(){
+    console.log('Listening at port '+port);
+});
---
+++ fanime/data.js
@@ -0,0 +1,196 @@
+let posts = [
+	{
+        'post-id': 0,
+    	'post-title': 'This is post 1',
+        'post-profile': 'https://i.pinimg.com/originals/14/b2/ca/14b2ca100b1b979983bdfdd24eac8a48.jpg',
+        'username': 'UserName *',
+        'post-time': 'posted 5hrs ago',
+        'post-desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex nobis expedita est iste, cumque unde quos omnis minus culpa et libero aperiam assumenda ut! Doloribus, ut obcaecati. Doloribus, assumenda praesentium.',
+    	'post-thumbs':{'up': 0, 'down': 0},
+        'post-image': '',
+        'post-comments':[
+            {
+                'commentUser': 'person 1',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 2',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 3',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 4',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 5',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },
+        ]
+	},{
+        'post-id': 1,
+    	'post-title': 'This is post 2',
+        'post-profile': 'https://i.pinimg.com/originals/14/b2/ca/14b2ca100b1b979983bdfdd24eac8a48.jpg',
+        'username': 'UserName *',
+        'post-time': 'posted 5hrs ago',
+        'post-desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex nobis expedita est iste, cumque unde quos omnis minus culpa et libero aperiam assumenda ut! Doloribus, ut obcaecati. Doloribus, assumenda praesentium.',
+    	'post-thumbs':{'up': 0, 'down': 0},
+        'post-image': 'https://cdn.pixabay.com/photo/2023/12/07/11/11/girl-8435340_1280.png',
+        'post-comments':[
+            {
+                'commentUser': 'person 1',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 2',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 3',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 4',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 5',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },
+        ]
+	},{
+        'post-id': 2,
+    	'post-title': 'This is post 3',
+        'post-profile': 'https://i.pinimg.com/originals/14/b2/ca/14b2ca100b1b979983bdfdd24eac8a48.jpg',
+        'username': 'UserName *',
+        'post-time': 'posted 5hrs ago',
+        'post-desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex nobis expedita est iste, cumque unde quos omnis minus culpa et libero aperiam assumenda ut! Doloribus, ut obcaecati. Doloribus, assumenda praesentium.',
+    	'post-thumbs':{'up': 0, 'down': 0},
+        'post-image': 'https://cdn.pixabay.com/photo/2023/12/07/11/11/girl-8435340_1280.png',
+        'post-comments':[
+            {
+                'commentUser': 'person 1',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 2',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 3',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 4',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 5',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },
+        ]
+	},{
+        'post-id': 3,
+    	'post-title': 'This is post 4',
+        'post-profile': 'https://i.pinimg.com/originals/14/b2/ca/14b2ca100b1b979983bdfdd24eac8a48.jpg',
+        'username': 'UserName *',
+        'post-time': 'posted 5hrs ago',
+        'post-desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex nobis expedita est iste, cumque unde quos omnis minus culpa et libero aperiam assumenda ut! Doloribus, ut obcaecati. Doloribus, assumenda praesentium.',
+    	'post-thumbs':{'up': 0, 'down': 0},
+        'post-image': 'https://cdn.pixabay.com/photo/2023/12/07/11/11/girl-8435340_1280.png',
+        'post-comments':[
+            {
+                'commentUser': 'person 1',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 2',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 3',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 4',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 5',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },
+        ]
+	},{
+        'post-id': 4,
+    	'post-title': 'This is post 5',
+        'post-profile': 'https://i.pinimg.com/originals/14/b2/ca/14b2ca100b1b979983bdfdd24eac8a48.jpg',
+        'username': 'UserName *',
+        'post-time': 'posted 5hrs ago',
+        'post-desc': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex nobis expedita est iste, cumque unde quos omnis minus culpa et libero aperiam assumenda ut! Doloribus, ut obcaecati. Doloribus, assumenda praesentium.',
+    	'post-thumbs':{'up': 0, 'down': 0},
+        'post-image': 'https://cdn.pixabay.com/photo/2023/12/07/11/11/girl-8435340_1280.png',
+        'post-comments':[
+            {
+                'commentUser': 'person 1',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 2',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 3',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 4',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },{
+                'commentUser': 'person 5',
+                'comment': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudantium eos aperiam vel. Labore enim officia illo, quibusdam culpa itaque unde vero amet.'
+            },
+        ]
+	}
+];
+
+let users = {
+    'person1' : {
+        'comments': [{
+            'post-id': posts[0]['post-id'],
+            'comment-title': 'comment1',
+            'comment-content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudant'
+            },
+            {
+            'comment-id': posts[1]['post-id'],
+            'comment-title': 'comment2',
+            'comment-content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudant'
+            },
+            {
+            'comment-id': posts[2]['post-id'],
+            'comment-title': 'comment3',
+            'comment-content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudant'
+            },
+            {
+            'comment-id': posts[3]['post-id'],
+            'comment-title': 'comment4',
+            'comment-content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudant'
+            },
+            {
+            'comment-id': posts[4]['post-id'],
+            'comment-title': 'comment5',
+            'comment-content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, eum! Libero natus quisquam sint dicta alias laudant'
+            }
+        ],
+        'posts': [posts[0], posts[1], posts[2], posts[3], posts[4]]
+    }
+};
+
+const data_friend = [
+	{
+    	'friend-name'   : 'Amy',
+    	'friend-img'	: 'https://cdn.pixabay.com/photo/2023/12/09/10/09/woman-8438999_960_720.png'
+	},{
+    	'friend-name'   : 'Cindy',
+    	'friend-img'	: 'https://cdn.pixabay.com/photo/2021/04/07/17/01/woman-6159648_960_720.jpg'
+	},{
+    	'friend-name'   : 'Emily',
+    	'friend-img'	: 'https://cdn.pixabay.com/photo/2021/10/08/13/02/woman-6691311_960_720.jpg'
+	},{
+    	'friend-name'   : 'Freya',
+    	'friend-img'	: 'https://cdn.pixabay.com/photo/2021/04/07/17/01/woman-6159648_960_720.jpg'
+	},{
+    	'friend-name'   : 'Gina',
+    	'friend-img'	: 'https://cdn.pixabay.com/photo/2021/10/08/13/02/woman-6691311_960_720.jpg'
+	},{
+    	'friend-name'   : 'Hellen',
+    	'friend-img'	: 'https://cdn.pixabay.com/photo/2023/12/09/10/09/woman-8438999_960_720.png'
+	}
+];
+
+module.exports.posts = posts;
+module.exports.users = users;
+module.exports.data_friend = data_friend;
