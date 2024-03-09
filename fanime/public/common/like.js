b66470b6a2d44559cb8ad672837ddf84fdac79e7
jongadmin <jongadmin@win-wks302390.77globalservices.com>
2024-03-10 04:21:06 +0800

    Fixed bugs on loading posts and viewing single post

--- fanime/public/common/like.js
+++ fanime/public/common/like.js
@@ -1,14 +1,34 @@
 $(document).ready(function(){
-    $("#like").click(function(){
-        
-
-        $.post('like', postData,
-        function(data, status){
-          if(status === 'success'){
-            let likes = parseInt($("#likes").text());
-            likes++;
-            $("#likes").text(likes);
-          }//if
-        });//fn+post
+    $(".like").click(function(){
+      let likeData = {
+        isLike: true,
+        postId: $(this).data('post-id')
+      }
+      $.post('like', likeData,
+      function(data, status){
+        if(status === 'success'){
+          let likes = parseInt($("#likes").text());
+          likes++;
+          $("#likes").text(likes);
+          console.log('Post liked!');
+        }//if
+      });//fn+post
     });//btn
+
+    $("#dislike").click(function(){
+      // Increment the value of dislikes
+      let dislikes = parseInt($("#dislikes").text());
+      dislikes++;
+      $("#dislikes").text(dislikes);
+
+      $.post('dislike', likeData, 
+      function(data, status){
+          if(status === 'success'){
+            let dislikes = parseInt($("#dislikes").text());
+            dislikes++;
+            $("#dislikes").text(dislikes);
+            console.log('Post disliked!');
+          }
+      });
+  });
 });//doc
