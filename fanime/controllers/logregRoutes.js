ab1af71bd483098a3d2d030dd874d716fab8aa75
Mark Jim <markedisonjim@gmail.com>
2024-03-26 17:12:59 +0800

    Pass hashing

--- fanime/controllers/logregRoutes.js
+++ fanime/controllers/logregRoutes.js
@@ -2,6 +2,8 @@ const responder = require('../models/Responder');
 const userModel = responder.userModel;
 const postModel = responder.postModel;
 const data = require('../data');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 function errorFn(err){
     console.log('Error found. Please trace!');
@@ -79,42 +81,48 @@ function add(server){
 
     //will fix later loading data json posts
     server.post('/register', function(req, resp){
-        const userInstance = userModel({
-            user: req.body.user,
-            email: req.body.email,
-            pass: req.body.pass,
-            profilepicture: 'defaultpfp.jpg',
-            profilebanner : 'defaultbanner.jpg',
-            userbio : 'Feel free to write your bio here!'
-        });
         const searchQuery = { email : req.body.email };
         userModel.findOne(searchQuery).then(function(user){
         if(user != undefined && user._id != null){
             resp.redirect('/registerFailed');
         }else{
+            let encrypted_pass = "";
+            bcrypt.hash(req.body.pass, saltRounds, function(err, hash) {
+                encrypted_pass = hash;
+                console.log("Encrypted pass: "+encrypted_pass);
+            });
+
+            const userInstance = userModel({
+                user: req.body.user,
+                email: req.body.email,
+                pass: encrypted_pass,
+                profilepicture: 'defaultpfp.jpg',
+                profilebanner : 'defaultbanner.jpg',
+                userbio : 'Feel free to write your bio here!'
+            });
             setLogIn(req.body.user, req.body.email, userInstance.profilepicture);
             userInstance.save().then(function(user) {
                 console.log('User created');
                 resp.redirect('/main');
                 }).catch(errorFn);
-        }
+            }
         });
     });
 
     server.post('/login', function(req, resp){
-        const searchQuery = {email : req.body.email, pass: req.body.pass};
+        const searchQuery = {email : req.body.email};
         userModel.findOne(searchQuery).then(function(user){
             if(user != undefined && user._id != null){
-                setLogIn(user.user, req.body.email, user.profilepicture);
-                resp.redirect('/main');
+                bcrypt.compare(req.body.vpass, encrypted_pass, function(err, result) {
+                    if(result){
+                        setLogIn(user.user, req.body.email, user.profilepicture);
+                        resp.redirect('/main');
+                    }else{
+                        resp.redirect('/loginFailed');
+                    }
+                });
             }else{
                 resp.redirect('/loginFailed');
-                // resp.render('unregMain',{
-                //     layout: 'index',
-                //     title: 'Unregistered Page',
-                //     posts: data.posts,
-                //     msg: 'Wrong Credentials, User does not exist...'
-                // });
             }
         });
     });
