0bf16c210aab50427cc98a301379451012dbadb3
jongadmin <jongadmin@win-wks302390.77globalservices.com>
2024-04-01 17:51:55 +0800

    added edit feature still incomplete

--- fanime/public/common/post.js
+++ fanime/public/common/post.js
@@ -68,6 +68,13 @@ $(document).ready(function(){
         console.log(postId);
         window.location.href = '/post?post_id=' + postId;
     });
+
+    $(".edit-post").click(function(event) {
+        event.preventDefault();
+        var postId = $(this).data("postid");
+        console.log(postId);
+        window.location.href = '/editpost?post_id=' + postId;
+    });
         
     $(".topic").click(function() {
         if($(this).val()==="true"){
@@ -87,4 +94,4 @@ $(document).ready(function(){
         }
     });
 
-});//doc
+});
