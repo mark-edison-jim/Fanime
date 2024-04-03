540b2a3e3c825468bcfdd747fb659e27957b6c92
mark-edison-jim <147977328+mark-edison-jim@users.noreply.github.com>
2024-04-03 14:15:43 +0800

    Update Responder.js

--- fanime/models/Responder.js
+++ fanime/models/Responder.js
@@ -8,19 +8,19 @@ const mongoStore = require('connect-mongodb-session')(session);
 const fs = require('fs');
 
 
-fs.readdir('./Images', (err, files) => { //Resets Images folder
-    console.log(files)
-    if (err) {
-        console.log(err);
-    }
-    files.forEach(file => {
-        const fileDir = path.join('./Images', file);
+// fs.readdir('./Images', (err, files) => { //Resets Images folder
+//     console.log(files)
+//     if (err) {
+//         console.log(err);
+//     }
+//     files.forEach(file => {
+//         const fileDir = path.join('./Images', file);
 
-        if (file !== 'defaultpfp.jpg' && file !== 'defaultbanner.jpg') {
-            fs.unlinkSync(fileDir);
-        }
-    });
-});
+//         if (file !== 'defaultpfp.jpg' && file !== 'defaultbanner.jpg') {
+//             fs.unlinkSync(fileDir);
+//         }
+//     });
+// });
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) =>{
